5b01235b9ec3ff467835c563e22be5f0f89885e0
ZRP <zrp1017@163.com>
2018-04-06 16:32:14 +0800

    Edit merge_wav

--- func/merge_wav.m
+++ func/merge_wav.m
@@ -5,9 +5,9 @@ end
 len1 = size(in1, 1);
 len2 = size(in2, 1);
 if len1 > len2
-    in2 = [in2; zeros(len1 - len2, size(in1, 1))];
+    in2 = [in2; zeros(len1 - len2, size(in1, 2))];
 elseif len1 < len2
-    in1 = [in1; zeros(len2 - len1, size(in1, 1))];
+    in1 = [in1; zeros(len2 - len1, size(in1, 2))];
 end
 out = in1 + in2;
 end
